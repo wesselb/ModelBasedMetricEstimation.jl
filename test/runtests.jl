b3658ca574df5a5176eb8a680d05ba6183fa4bfb
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-08-31 13:39:26 +0100

    Freeze the RNG so can't get unlucky during tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using FiniteDifferences
 using Metrics
 using Quadrature
+using Random
 using Statistics
 using ModelBasedMetricEstimation
 using Test
 
+Random.seed!(1)
 @testset "ModelBasedMetricEstimation.jl" begin
     include("model.jl")
     include("estimate.jl")
